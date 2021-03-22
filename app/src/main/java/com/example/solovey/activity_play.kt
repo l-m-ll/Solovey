f746f5de8f2d8eaa55b217b285a7307882684125
liza <gorbunova.elizaveta@gmail.com>
2021-03-22 12:46:53 +0200

    add scroll to player

--- app/src/main/java/com/example/solovey/activity_play.kt
+++ app/src/main/java/com/example/solovey/activity_play.kt
@@ -1,10 +1,16 @@
 package com.example.solovey
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.view.View
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_play.*
+import kotlin.math.round
 
 class activity_play : AppCompatActivity() {
     private lateinit var mp3: MediaPlayer
@@ -13,10 +19,53 @@ class activity_play : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
         mp3 = MediaPlayer.create(this, R.raw.solovey)
-        mp3.start()
+        timeBar.max = (mp3.duration / 1000).toInt()
+        timeBar.progress = 0
         mp3.isLooping = true
         mp3.setVolume(0.5f, 0.5f)
-        timePassed = mp3.duration
+        mp3.start()
+
+
+        timeBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                   mp3.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                println(seekBar.progress)
+                mp3.seekTo(seekBar.progress * 1000)
+                timeBar.progress = seekBar.progress
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                println(seekBar.progress)
+                mp3.seekTo(seekBar.progress * 1000)
+                timeBar.progress = seekBar.progress
+            }
+        })
+
+        Thread(Runnable {
+            while(mp3 != null) {
+                try {
+                    var msg = Message()
+                    msg.what = mp3.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+
+                }
+            }
+        }).start()
+    }
+
+    @SuppressLint("HandlerLint")
+    var handler = object: Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            var currentPosition = msg.what
+            timeBar.progress = (currentPosition / 1000).toInt()
+        }
     }
 
     fun clickPlayBtn(v : View) {
